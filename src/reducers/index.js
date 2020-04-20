d674f34590385f0d5af15ea2311d105eedc594c3
Arjan <janjanarnaldo@gmail.com>
2020-04-20 10:37:14 +0800

    Oopsie

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,5 @@
-import { combineReducers } from 'redux'
-import { reducer as form } from 'redux-form'
+import { combineReducers } from 'redux';
+import { reducer as form } from 'redux-form';
 import users from './users';
 import posts from './posts';
 
--- src/utils/avatarHelper.js
+++ src/utils/avatarHelper.js
@@ -1,6 +1,6 @@
 import { AVATARS } from '../constants/avatars';
 
 export const randomAvatar = () => {
-  const randomKey = Math.floor(Math.random() * 7);
+  const randomKey = Math.floor(Math.random() * AVATARS.length);
   return AVATARS[randomKey];
 }
